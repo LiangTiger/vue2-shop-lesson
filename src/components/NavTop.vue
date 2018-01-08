0040d87070e6935072c3340ac02aafce5f555945
LiangTiger <740959030@qq.com>
2018-01-08 19:40:14 +0800

    nav图标完善

--- src/components/NavTop.vue
+++ src/components/NavTop.vue
@@ -1,25 +1,36 @@
 <template>
  <div class="topBar">
     <div class="topbarNav">
-        <a href="#">小米商城</a><span> | </span>
-        <a href="#">MIUI</a><span> | </span>
-        <a href="#">loT</a><span> | </span>
-        <a href="#">云服务</a><span> | </span>
-        <a href="#">水滴</a><span> | </span>
-        <a href="#">金融</a><span> |</span>
-        <a href="#">有品</a><span> | </span>
-        <a href="#">Select Region</a>
+        <div class="topbarNavLeft">
+            <a href="#">小米商城</a><span>|</span>
+            <a href="#">MIUI</a><span>|</span>
+            <a href="#">loT</a><span>|</span>
+            <a href="#">云服务</a><span>|</span>
+            <a href="#">水滴</a><span>|</span>
+            <a href="#">金融</a><span>|</span>
+            <a href="#">有品</a><span>|</span>
+            <a href="#">Select Region</a>
+        </div>
+        <div class="topbarNavCart">
+           
+            <a href="#"> <i class="iconfont icon-cart"></i><span>购物车({{cartSum}})</span></a>
+        </div>
+        <div class="topbarNavRight">
+            <a href="#">登录</a><span>|</span>
+            <a href="#">注册</a><span>|</span>
+            <a href="#">消息通知</a>
+        </div>
     </div>
-    <div></div>
-    <div></div>
  </div>
 </template>
 
 <script>
+import './../assets/css/base.css'
+import './../assets/css/iconfont/iconfont.css'
  export default {
    data () {
      return {
-         
+         cartSum:'1'
      }
    },
  }
@@ -33,7 +44,40 @@
         background:#333;
     }
     .topbarNav{
-        line-height:40px
+        max-width:1280px;
+        margin:0 auto;
+    }
+    .topbarNavLeft{
+        float:left;
+    }
+    .topbarNavRight{
+        float:right;
+    }
+    .topbarNavCart{
+        width:120px;
+        height:40px;
+        float:right;
+        margin-left:20px;
+        text-align:center;
+    }
+    .topbarNavCart a{
+        display:block;
+        line-height:40px;
+        background:#424242;
+
+    }
+    .iconfont{
+        margin-right:10px
+    }
+    .topbarNavRight span{
+        line-height:40px;
+        color:#424242;
+        margin:0.5em;
+    }
+    .topbarNavLeft span{
+        line-height:40px;
+        color:#424242;
+        margin:0.5em;
     }
  
 </style>
